88beff170f46ff6521b43c3ea336c82b2954e789
vusqa12 <you@vusqa1999@gmail.com>
2021-12-05 19:07:30 +0400

    calculator

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace calculatar
+{
+    class Program:Class1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Toplama emelyati ucun + dail edin");
+            Console.WriteLine("cixma emelyati ucun - dail edin");
+            Console.WriteLine("vurma emelyati ucun * dail edin");
+            Console.WriteLine("bolme emelyati ucun / dail edin");
+
+            Class1 cixma = new Class1();
+           
+        }
+        
+       
+
+    }
+}
